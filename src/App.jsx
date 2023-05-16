eee1100357760f254be3e3f14f9ce5dc3242a3c1
João Pedro Caetano <caetanojpo@gmail.com>
2023-05-15 23:15:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Index from "./pages/Index";
+
+
+export default function App() {
+
+
+  return (
+    <>
+      <Index />
+    </>
+  )
+}
+
---
+++ src/pages/Index/index.jsx
@@ -0,0 +1,19 @@
+import Banner from "../../components/Banner";
+import Header from "../../components/Header";
+import Menu from "../../components/Menu";
+
+import styles from './Index.module.scss'
+
+export default function Index() {
+    return (
+        <>
+            <Header />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <Banner />
+                </section>
+            </main>
+        </>
+    )
+}
