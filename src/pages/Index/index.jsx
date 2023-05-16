a4323c5cb2f87b6c945e948fcb054894378528d9
João Pedro Caetano <caetanojpo@gmail.com>
2023-05-15 23:27:14 -0300

    Fonts

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,7 +1,6 @@
 import Banner from "../../components/Banner";
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
-
 import styles from './Index.module.scss'
 
 export default function Index() {
